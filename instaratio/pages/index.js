5bb8b99382257565d1a2e93a8c1ec9d4ee6d945b
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2023-09-25 12:13:44 +0530

    removed facebook login

--- instaratio/pages/index.js
+++ instaratio/pages/index.js
@@ -1,10 +1,5 @@
 import { Inter } from "next/font/google";
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
-const inter = Inter({ subsets: ["latin"] });
 
-const responseFacebook = (response) => {
-	console.log(response);
-};
 
 export default function Home() {
 	return (
